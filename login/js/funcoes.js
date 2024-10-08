61d97f950590345a1a05bde8af6381f85bd081c4
eriksoncampos <84237867+eriksoncampos@users.noreply.github.com>
2024-10-08 18:56:36 -0300

    Add files via upload

---
+++ login/js/funcoes.js
@@ -0,0 +1,23 @@
+//Inicialização de variáveis.
+const usuarios = [
+    {id:1,usuario:"arildo",senha:"s123"},
+    {id:2,usuario:"brildo",senha:"s456"},
+];
+
+function exibir(){
+    let usuarioPermitido = false;
+    const campoUsuario = document.getElementById("usuario");
+    const campoSenha = document.getElementById("senha");
+    const valor = campoUsuario.value;
+    const senha = campoSenha.value;
+
+    //Validar se os campos estão preenchidos.
+    
+    for(let usuario of usuarios){
+            if(usuario.usuario === valor && usuario.senha === senha){
+                usuarioPermitido = true;
+            }
+    };    
+
+    document.getElementById("exibemensagem").innerHTML = usuarioPermitido ? "Acesso permitido." : "Acesso negado."
+};
